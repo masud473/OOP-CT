a54343a9bf86dbd4b812c7e4008b85f4e7fe2dba
Md. Farhan Masud <112246492+masud473@users.noreply.github.com>
2025-10-25 17:29:02 +0600

    Java Converter

---
+++ Convert.java
@@ -0,0 +1,52 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class Convert implements ActionListener {
+    JFrame frame = new JFrame("Convert");
+    JLabel l1 = new JLabel("Foot");
+    JLabel l2 = new JLabel("Inch");
+    JTextField foot = new JTextField("0.00");
+    JTextField inch = new JTextField("0.00");
+    JButton b = new JButton("Convert to inch");
+
+    Convert() {
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+
+        l1.setBounds(10, 10, 40, 30);
+        foot.setBounds(60, 10, 150, 30);
+        b.setBounds(40, 50, 140, 30);
+        l2.setBounds(10, 90, 40, 30);
+        inch.setBounds(60, 90, 150, 30);
+
+        inch.setEditable(false);
+        b.addActionListener(this);
+
+        frame.add(foot);
+        frame.add(l1);
+        frame.add(l2);
+        frame.add(b);
+        frame.add(inch);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == b) {
+            Double d = Double.valueOf(foot.getText());// extremely important conversion
+            d *= 12;
+            inch.setText(d + "");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Convert();
+    }
+}
