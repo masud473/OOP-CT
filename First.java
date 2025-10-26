1a5533b7cc451ad04140b95a2be5278bf562bc91
Md. Farhan Masud <112246492+masud473@users.noreply.github.com>
2025-10-26 09:12:43 +0600

    more functionalities of inheritance

--- First.java
+++ First.java
@@ -1,24 +1,43 @@
-abstract class A {
-    abstract void func();
+interface A {
+    void func();
 
-    void display() {
+    static void printStatic() {
+        System.out.println("This is a static method in interface A");
+    }
+
+    default void display() {
         System.out.println("This is a display method");
 
     }
 }
 
-class B extends A {
+abstract class A1 {
+    abstract void func2();
+
+}
+
+class B extends A1 implements A {
     @Override
-    void func() {
+    public void func() {
         System.out.println("This is func from B");
     }
+
+    void uniqueBMethod() {
+        System.out.println("This is a unique method in B");
+    }
+
+    @Override
+    public void func2() {
+        System.out.println("This is func2 from B");
+    }
+
 }
 
 class C extends B {
     String val;
 
     @Override
-    void func() {
+    public void func() {
         System.out.println("This is func from C");
     }
 
@@ -31,9 +50,18 @@ class C extends B {
 public class First {
     public static void main(String[] args) {
         A a = new C();
+        A.printStatic();
         a.func();
         a.display();
-        ((C) a).val = "";// use typecasting to access child methods
-        System.out.println(((C)a).val);
+        ((C) a).uniqueBMethod();
+        ((C) a).val = "c";// use typecasting to access child methods+data
+        ((C) a).func2();
+        System.out.println(((C) a).val);
+        B b = new C();
+        b.func();
+        b.display();
+        b.uniqueBMethod();
+        ((C) b).uniqueCMethod();
+
     }
 }
